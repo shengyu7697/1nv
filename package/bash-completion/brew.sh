76d4bf6e252ac948512d7c342e2fe78d6c8c60f6
shengyu <shengyu7697@gmail.com>
2015-01-11 23:53:39 +0800

    Add bash-completion

---
+++ package/bash-completion/brew.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ ! -f /usr/local/etc/bash_completion ]; then
+    brew install bash-completion
+else
+    echo "\033[31;4mWarning\033[0m: package already installed"
+fi
---
+++ package/bash-completion/yum.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ ! -f /usr/share/bash-completion/bash_completion ]; then
+    sudo yum install -y bash-completion
+else
+    echo "\033[31;4mWarning\033[0m: package already installed"
+fi
