a9c5a58fb227f520015224a25390655e01ace6b7
shengyu <shengyu7697@gmail.com>
2015-01-15 20:07:22 +0800

    Change /usr/local to brew --prefix

--- package/bash-completion/brew.sh
+++ package/bash-completion/brew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f /usr/local/etc/bash_completion ]; then
+if [ ! -f `brew --prefix`/etc/bash_completion ]; then
     brew install bash-completion
 else
     echo -e "\033[31;4mWarning\033[0m: package already installed"
