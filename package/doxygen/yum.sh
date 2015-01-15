dadc6705db428829eb49321e18c07dca00ecc259
shengyu <shengyu7697@gmail.com>
2015-01-15 20:16:56 +0800

    Fix no -y option

--- package/doxygen/yum.sh
+++ package/doxygen/yum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! command -v doxygen > /dev/null; then
-    sudo yum install doxygen
+    sudo yum install -y doxygen
 else
     echo -e "\033[31;4mWarning\033[0m: package already installed"
 fi
