b345d82c1f0382522fc57d26400568faa45a252b
shengyu <shengyu7697@gmail.com>
2015-01-15 20:19:45 +0800

    Fix no -y option

--- package/doxygen/apt.sh
+++ package/doxygen/apt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! command -v doxygen > /dev/null; then
-    sudo apt-get install doxygen
+    sudo apt-get install -y doxygen
 else
     echo -e "\033[31;4mWarning\033[0m: package already installed"
 fi
