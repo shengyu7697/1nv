e3d655edc1c135c5847b8538d862fd13ff2ee314
shengyu <shengyu7697@gmail.com>
2015-01-20 22:19:17 +0800

    Update exfat

--- package/exfat/apt.sh
+++ package/exfat/apt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! command -v mkfs.exfat > /dev/null; then
-    sudo apt-get install -y exfat-fuse
+    sudo apt-get install -y exfat-fuse exfat-utils
 else
     echo -e "\033[31;4mWarning\033[0m: package already installed"
 fi
