8561b7490947edfd80d868611af436db8063d47e
shengyu <shengyu7697@gmail.com>
2015-01-24 22:39:19 +0800

    Remove $START_SCRIPT and $SETUP_SCRIPT

--- install.sh
+++ install.sh
@@ -15,8 +15,6 @@ elif [ "$OS" == "Darwin" ]; then
     DISTRO="Mac OSX"
     INSTALL_SCRIPT="brew.sh"
 fi
-START_SCRIPT=start.sh
-SETUP_SCRIPT=setup.sh
 
 while read line 
 do
@@ -32,16 +30,8 @@ do
         cd $NAME
         if [ -f $INSTALL_SCRIPT ]; then
             ./$INSTALL_SCRIPT | tee /tmp/install.log
-
-            if [ -f $START_SCRIPT ]; then
-                ./$START_SCRIPT
-            fi
         else
             echo -e "\033[31;4mWarning\033[0m: no $INSTALL_SCRIPT file"
-        fi
-
-        if [ -f $SETUP_SCRIPT ]; then
-            ./$SETUP_SCRIPT
         fi
 
         cd - > /dev/null
